9d0f26e9a553cc7d4bade4f44dfdc3887644cb12
Ade Fathoni Prastya <adefathoniprastya@gmail.com>
2025-08-02 21:22:48 +0700

    feat: add verification metadata for Google in RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,10 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 export const metadata: Metadata = {
 	title: "LokaKulon",
 	description:
-		"Katalog UMKM Kelurahan Lidah Kulon, informasi tentang usaha kecil dan menengah yang berada di Kelurahan Lidah Kulon, Kecamatan Lakarsantri, Kota Surabaya, Jawa Timur."
+		"Katalog UMKM Kelurahan Lidah Kulon, informasi tentang usaha kecil dan menengah yang berada di Kelurahan Lidah Kulon, Kecamatan Lakarsantri, Kota Surabaya, Jawa Timur.",
+	verification: {
+		google: "K3se7__XI3jEmXrbJBsOQ6UnzrKkSBNxdAZBKvgtpts"
+	}
 };
 
 export default function RootLayout({
